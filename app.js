48b7f98552476e228148c32214881c9ef95ba404
Madden85 <mohdeizudinsyah@gmail.com>
2026-07-01 13:32:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -578,7 +578,7 @@ function ensurePaymentModal() {
         <button id="closePaymentModal" class="x" type="button" aria-label="Tutup">×</button>
         <img id="payLogo" class="modal-logo" src="${attr(CONFIG.LOGO_IMAGE || "numologo.png")}" alt="Logo">
         <h3>Bayar Sekarang</h3>
-        <p>Sila isi detail dibawah dan tekan button Teruskan pembayaran</p>
+        <p>Sila isi detail dibawah</p>
 
         <div class="order-summary">
           <div class="row"><span>Produk</span><strong id="payProduct">-</strong></div>
